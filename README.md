5597351b373487ba59a7ad7f01a8b6688aec91fd
jrueda <jrueda@DeveloperPrime.(none)>
2015-07-11 10:25:28 -0400

    BSD license notice included

--- README.md
+++ README.md
@@ -1,5 +1,70 @@
-#FixPrime
+# FixPrime
 
-##Fix Protocol Engine
+FixPrime is a project (still under development), whose scope is to provide a very flexible framework to develop applications based in the fix protocol (Financial Information Exchange). 
+###Currently available features in test:
+ - Full customization of the specification of all fix protocol versions.
+ - Message parser.
+ - Message serializator.
+ - Unlimited Nesting of repeating groups.
 
-**Status:** In Development
+###Complete set of features planned in the project scope:
+  - Full customization of the specification of all fix protocol versions.
+  - Message parser.
+  - Message serializator.
+  - Customization of callback for Fix protocol events.
+  - Automatic message sequencing.
+  - Automatic session layer interaction.
+  - Full message's hierarchies navigation.
+  - Unlimited Nesting of repeating groups.
+  - Automatic validation of Fix protocol datatypes.
+  - Automatic validation of field's valid values.
+  - Definition of multiple session engines
+  - Behavior as Acceptor or initiator.
+ 
+###Project's dependencies
+[boost libraries] - Release 1.58.0
+Download and install the boost libraries following the [boost libraries]'s site instructions.
+
+[Jsoncpp] - Even though the project uses jsoncpp, you don't need to do anything about it, because the project has included the amalgamated version of jsoncpp source code.
+
+###Quick Start using Fix Prime
+
+``` c++  
+#include "FixDictionary.h"
+using namespace std;
+using namespace fprime;
+int main(int argc, char** argv) {
+    try {
+        /* 
+         * Instantiate  the fix dictionary.
+         * You can uses a pointer to the dictionary and load all protocol 
+         * specifications only once in order to be used  in all application 
+         * contexts.
+         * 
+         * Other opcion is instantiate directly a instance of the dictionary 
+         * like this:
+         *  
+         * FixDictionary myFixDictionary;
+         */
+        FixDictionary::FixDictionaryPtr fixDictionaryPtr(new FixDictionary);
+        /*
+         * Load the procolos defined on the folder FixSpecifications.
+         * The file FixVersion contains the path for every fix protocol specification
+         * Replace yourpath for your propper path in your operating system
+         */
+        fixDictionaryPtr->loadProtocols("yourpath/FixPrime/FixSpecifications/FixVersions.json");
+        /*
+         * Use the desired protocol version
+         */
+        Protocol::ProtocolPtr protocolPtr = fixDictionaryPtr->getProtocol("FIX.4.4");
+        cout << "Protocols loaded sucessfully." << endl;
+        return 0;
+    } catch (const exception& e) {
+        cout << "An exception has ocurred loading fix protocols: " << e.what() << endl;
+    }
+```
+
+
+
+[boost libraries]:http://www.boost.org
+[Jsoncpp]:https://github.com/open-source-parsers/jsoncpp
--- nbproject/Makefile-Debug.mk
+++ nbproject/Makefile-Debug.mk
@@ -69,81 +69,81 @@ LDLIBSOPTIONS=-lboost_system
 
 # Build Targets
 .build-conf: ${BUILD_SUBPROJECTS}
-	"${MAKE}"  -f nbproject/Makefile-${CND_CONF}.mk ${TESTDIR}/TestFiles/f1
+	"${MAKE}"  -f nbproject/Makefile-${CND_CONF}.mk ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/libsFixPrime.${CND_DLIB_EXT}
 
-${TESTDIR}/TestFiles/f1: ${OBJECTFILES}
-	${MKDIR} -p ${TESTDIR}/TestFiles
-	${LINK.cc} -o ${TESTDIR}/TestFiles/f1 ${OBJECTFILES} ${LDLIBSOPTIONS} -shared -fPIC
+${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/libsFixPrime.${CND_DLIB_EXT}: ${OBJECTFILES}
+	${MKDIR} -p ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}
+	${LINK.cc} -o ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/libsFixPrime.${CND_DLIB_EXT} ${OBJECTFILES} ${LDLIBSOPTIONS} -shared -fPIC
 
 ${OBJECTDIR}/src/Component.o: src/Component.cpp 
 	${MKDIR} -p ${OBJECTDIR}/src
 	${RM} "$@.d"
-	$(COMPILE.cc) -g -I/usr/local/include -Iinclude -I. -I. -std=c++11 -fPIC  -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/src/Component.o src/Component.cpp
+	$(COMPILE.cc) -g -I/usr/local/include -Iinclude -std=c++11 -fPIC  -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/src/Component.o src/Component.cpp
 
 ${OBJECTDIR}/src/DataHolderFactory.o: src/DataHolderFactory.cpp 
 	${MKDIR} -p ${OBJECTDIR}/src
 	${RM} "$@.d"
-	$(COMPILE.cc) -g -I/usr/local/include -Iinclude -I. -I. -std=c++11 -fPIC  -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/src/DataHolderFactory.o src/DataHolderFactory.cpp
+	$(COMPILE.cc) -g -I/usr/local/include -Iinclude -std=c++11 -fPIC  -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/src/DataHolderFactory.o src/DataHolderFactory.cpp
 
 ${OBJECTDIR}/src/Field.o: src/Field.cpp 
 	${MKDIR} -p ${OBJECTDIR}/src
 	${RM} "$@.d"
-	$(COMPILE.cc) -g -I/usr/local/include -Iinclude -I. -I. -std=c++11 -fPIC  -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/src/Field.o src/Field.cpp
+	$(COMPILE.cc) -g -I/usr/local/include -Iinclude -std=c++11 -fPIC  -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/src/Field.o src/Field.cpp
 
 ${OBJECTDIR}/src/FixDictionary.o: src/FixDictionary.cpp 
 	${MKDIR} -p ${OBJECTDIR}/src
 	${RM} "$@.d"
-	$(COMPILE.cc) -g -I/usr/local/include -Iinclude -I. -I. -std=c++11 -fPIC  -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/src/FixDictionary.o src/FixDictionary.cpp
+	$(COMPILE.cc) -g -I/usr/local/include -Iinclude -std=c++11 -fPIC  -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/src/FixDictionary.o src/FixDictionary.cpp
 
 ${OBJECTDIR}/src/FixLoader.o: src/FixLoader.cpp 
 	${MKDIR} -p ${OBJECTDIR}/src
 	${RM} "$@.d"
-	$(COMPILE.cc) -g -I/usr/local/include -Iinclude -I. -I. -std=c++11 -fPIC  -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/src/FixLoader.o src/FixLoader.cpp
+	$(COMPILE.cc) -g -I/usr/local/include -Iinclude -std=c++11 -fPIC  -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/src/FixLoader.o src/FixLoader.cpp
 
 ${OBJECTDIR}/src/FixParser.o: src/FixParser.cpp 
 	${MKDIR} -p ${OBJECTDIR}/src
 	${RM} "$@.d"
-	$(COMPILE.cc) -g -I/usr/local/include -Iinclude -I. -I. -std=c++11 -fPIC  -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/src/FixParser.o src/FixParser.cpp
+	$(COMPILE.cc) -g -I/usr/local/include -Iinclude -std=c++11 -fPIC  -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/src/FixParser.o src/FixParser.cpp
 
 ${OBJECTDIR}/src/IntDataHolder.o: src/IntDataHolder.cpp 
 	${MKDIR} -p ${OBJECTDIR}/src
 	${RM} "$@.d"
-	$(COMPILE.cc) -g -I/usr/local/include -Iinclude -I. -I. -std=c++11 -fPIC  -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/src/IntDataHolder.o src/IntDataHolder.cpp
+	$(COMPILE.cc) -g -I/usr/local/include -Iinclude -std=c++11 -fPIC  -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/src/IntDataHolder.o src/IntDataHolder.cpp
 
 ${OBJECTDIR}/src/Message.o: src/Message.cpp 
 	${MKDIR} -p ${OBJECTDIR}/src
 	${RM} "$@.d"
-	$(COMPILE.cc) -g -I/usr/local/include -Iinclude -I. -I. -std=c++11 -fPIC  -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/src/Message.o src/Message.cpp
+	$(COMPILE.cc) -g -I/usr/local/include -Iinclude -std=c++11 -fPIC  -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/src/Message.o src/Message.cpp
 
 ${OBJECTDIR}/src/Node.o: src/Node.cpp 
 	${MKDIR} -p ${OBJECTDIR}/src
 	${RM} "$@.d"
-	$(COMPILE.cc) -g -I/usr/local/include -Iinclude -I. -I. -std=c++11 -fPIC  -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/src/Node.o src/Node.cpp
+	$(COMPILE.cc) -g -I/usr/local/include -Iinclude -std=c++11 -fPIC  -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/src/Node.o src/Node.cpp
 
 ${OBJECTDIR}/src/Protocol.o: src/Protocol.cpp 
 	${MKDIR} -p ${OBJECTDIR}/src
 	${RM} "$@.d"
-	$(COMPILE.cc) -g -I/usr/local/include -Iinclude -I. -I. -std=c++11 -fPIC  -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/src/Protocol.o src/Protocol.cpp
+	$(COMPILE.cc) -g -I/usr/local/include -Iinclude -std=c++11 -fPIC  -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/src/Protocol.o src/Protocol.cpp
 
 ${OBJECTDIR}/src/StringDataHolder.o: src/StringDataHolder.cpp 
 	${MKDIR} -p ${OBJECTDIR}/src
 	${RM} "$@.d"
-	$(COMPILE.cc) -g -I/usr/local/include -Iinclude -I. -I. -std=c++11 -fPIC  -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/src/StringDataHolder.o src/StringDataHolder.cpp
+	$(COMPILE.cc) -g -I/usr/local/include -Iinclude -std=c++11 -fPIC  -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/src/StringDataHolder.o src/StringDataHolder.cpp
 
 ${OBJECTDIR}/src/UIntDataHolder.o: src/UIntDataHolder.cpp 
 	${MKDIR} -p ${OBJECTDIR}/src
 	${RM} "$@.d"
-	$(COMPILE.cc) -g -I/usr/local/include -Iinclude -I. -I. -std=c++11 -fPIC  -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/src/UIntDataHolder.o src/UIntDataHolder.cpp
+	$(COMPILE.cc) -g -I/usr/local/include -Iinclude -std=c++11 -fPIC  -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/src/UIntDataHolder.o src/UIntDataHolder.cpp
 
 ${OBJECTDIR}/src/Value.o: src/Value.cpp 
 	${MKDIR} -p ${OBJECTDIR}/src
 	${RM} "$@.d"
-	$(COMPILE.cc) -g -I/usr/local/include -Iinclude -I. -I. -std=c++11 -fPIC  -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/src/Value.o src/Value.cpp
+	$(COMPILE.cc) -g -I/usr/local/include -Iinclude -std=c++11 -fPIC  -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/src/Value.o src/Value.cpp
 
 ${OBJECTDIR}/src/jsoncpp.o: src/jsoncpp.cpp 
 	${MKDIR} -p ${OBJECTDIR}/src
 	${RM} "$@.d"
-	$(COMPILE.cc) -g -I/usr/local/include -Iinclude -I. -I. -std=c++11 -fPIC  -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/src/jsoncpp.o src/jsoncpp.cpp
+	$(COMPILE.cc) -g -I/usr/local/include -Iinclude -std=c++11 -fPIC  -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/src/jsoncpp.o src/jsoncpp.cpp
 
 # Subprojects
 .build-subprojects:
@@ -151,7 +151,7 @@ ${OBJECTDIR}/src/jsoncpp.o: src/jsoncpp.cpp
 # Clean Targets
 .clean-conf: ${CLEAN_SUBPROJECTS}
 	${RM} -r ${CND_BUILDDIR}/${CND_CONF}
-	${RM} ${TESTDIR}/TestFiles/f1
+	${RM} ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/libsFixPrime.${CND_DLIB_EXT}
 
 # Subprojects
 .clean-subprojects:
